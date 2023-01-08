c7ca3adb81c51a8ce407c761daa65d0fba3a4125
Riggy0508 <rigved.2017@gmail.com>
2023-01-07 16:39:18 -0800

    Added Comments

--- public/script.js
+++ public/script.js
@@ -1,10 +1,13 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
+
+//The below peer API helps us to listen to different uesr's that are connected to our port 3001, which will helps us connect with other peers available on the network
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001',
 });
 const myVideo = document.createElement('video');
+//We don't wanna listen to our own video while been on a call. So as soon as we join a room we are gonna make ourself mute.
 myVideo.muted = true;
 const peers = {};
 navigator.mediaDevices
@@ -22,16 +25,17 @@ navigator.mediaDevices
         addVideoStream(video, userVideoStream);
       });
     });
-
+    // below code let's us know when a new user have connected and once connected we are gonna fetch there video and audio via calling the function mentioned
     socket.on('user-connected', (userId) => {
       connectToNewUser(userId, stream);
     });
   });
-
+//The below code makes sure that once a user is disconnected from a stream we will no longer be able to see that user's stream onto the webpage
 socket.on('user-disconnected', (userId) => {
   if (peers[userId]) peers[userId].close();
 });
 
+//This part of the code helps us in identifing a new user id and help us in joining that user to a new room.
 myPeer.on('open', (id) => {
   socket.emit('join-room', ROOM_ID, id);
 });
--- server.js
+++ server.js
@@ -7,18 +7,28 @@ const server = require('http').Server(app);
 // We are using socket io so that it can handle the communication between the server
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
+//console.log(uuidV4);
 
+//the below let's us know how we are gonna handle the view's in our case we have used ejs in order to do so.
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`);
-});
+//Working on the router to our homepage, we are now gonna use the route and requets and order to get the response from the server
 
+app.get('/', (req, res) => {
+  //here we will get the dynamic room id in order for our user's to enter it.
+  //res.redirect(`/${roomId}`)
+  res.redirect(`/${uuidV4()}`);
+  //example: 1. 983676b3-df1c-4f1b-85ac-d93f3dd50d0e   2. ae6c56a3-c7a7-477b-9421-80f0dc65294c
+});
+//Setting up the room for our applications
 app.get('/:room', (req, res) => {
   res.render('room', { roomID: req.params.room });
 });
 
+//the below socket connections notifies us each time someone tries to enter our room. We have set different event's in order to handle different sceneario
+//for our below event we are gonna listen to our socket if someone join's a room and if a user join a room we are gonna use that room-id and user-id in order to
+// display there respected video via calling methods from our script.js file
 io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
@@ -30,4 +40,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000);
+server.listen(3000); // this allows our server to listen to our request at port 3000. ex: localhost:3000
+// By using the above method o≈ne can notice that even when the server is not running we will still be communicate with other
+//people as it directly communicate's with the person's computer. So we don't have to worry about sending the traffic through server
+// Sever is just used to create the room's
